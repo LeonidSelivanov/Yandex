7b39c7b2aa5ff8c8a7d7d52d90984cbada3d6da5
Leonid <161122560+LeonidSelivanov@users.noreply.github.com>
2024-10-25 18:09:08 +0500

    remove rep

---
+++ task1/main_test.go
@@ -0,0 +1,39 @@
+package task
+
+import (
+	"errors"
+	"testing"
+)
+
+type Test struct {
+	in  string
+	out float64
+	err error
+}
+
+func TestCalc(t *testing.T) {
+	tests := []Test{
+		{
+			in:  "1+1",
+			out: 2,
+			err: nil,
+		},
+		{
+			in:  "5*5-(5*5)",
+			out: 0,
+			err: nil,
+		},
+		{
+			in: "",
+			out: 0,
+			err: errors.New("ErrFoo Emmpty expression"),
+		},
+	}
+	for _, test := range tests {
+		gotOut, gotErr := test.out, test.err
+		expectedOut, expectedOutErr := Calc(test.in)
+		if gotOut != expectedOut || gotErr != expectedOutErr {
+			t.Fatalf("Error!")
+		}
+	}
+}
